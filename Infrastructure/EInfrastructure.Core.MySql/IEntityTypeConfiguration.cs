845bd87f48986599a3d11692b95a97a2fdb546f5
zhenlei <839238345@qq.com>
2018-06-26 23:19:34 +0800

    增加mysql扩展

---
+++ Infrastructure/EInfrastructure.Core.MySql/IEntityTypeConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EInfrastructure.Core.MySql
+{
+    public interface IEntityMap<TEntityType> where TEntityType : class
+    {
+        void Map(EntityTypeBuilder<TEntityType> builder);
+    }
+}
--- Infrastructure/EInfrastructure.Core/Ddd/IQuery.cs
+++ Infrastructure/EInfrastructure.Core/Ddd/IQuery.cs
@@ -6,20 +6,20 @@ using EInfrastructure.Core.Data;
 
 namespace EInfrastructure.Core.Ddd
 {
-    public interface IQuery<TEntity,T> where TEntity : IEntity<T>
-    {
-        TEntity GetOne(Expression<Func<TEntity, bool>> condition);
+  public interface IQuery<TEntity, T> where TEntity : IEntity<T>
+  {
+    TEntity GetOne(Expression<Func<TEntity, bool>> condition, bool isTracking = true);
 
-        PageData<TEntity> GetList(Expression<Func<TEntity, bool>> condition, int pagesize, int pageindex, bool istotal);
+    PageData<TEntity> GetList(Expression<Func<TEntity, bool>> condition, int pagesize, int pageindex, bool istotal);
 
-        List<TEntity> GetList(Expression<Func<TEntity, bool>> condition);
+    List<TEntity> GetList(Expression<Func<TEntity, bool>> condition);
 
-        bool Exists(Expression<Func<TEntity, bool>> condition);
+    bool Exists(Expression<Func<TEntity, bool>> condition);
 
-        List<TEntity> TopN(Expression<Func<TEntity, bool>> condition, int topN);
+    List<TEntity> TopN(Expression<Func<TEntity, bool>> condition, int topN);
 
-        IQueryable<TEntity> GetListQuery(Expression<Func<TEntity, bool>> condition);
+    IQueryable<TEntity> GetListQuery(Expression<Func<TEntity, bool>> condition);
 
-        IQueryable<TEntity> GetQueryable();
-    }
+    IQueryable<TEntity> GetQueryable();
+  }
 }
