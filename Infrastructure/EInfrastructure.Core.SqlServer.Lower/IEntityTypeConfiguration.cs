0b023430b9deead9dca93865677c665ccc44e928
zhenlei <839238345@qq.com>
2018-10-20 17:13:44 +0800

    删除低版本引用的efcore

--- Infrastructure/EInfrastructure.Core.SqlServer.Lower/IEntityTypeConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-namespace EInfrastructure.Core.SqlServer.Lower
-{
-    public interface IEntityMap<TEntityType> where TEntityType : class
-    {
-        void Map(EntityTypeBuilder<TEntityType> builder);
-    }
-}
