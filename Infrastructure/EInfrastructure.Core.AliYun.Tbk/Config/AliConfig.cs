e9e33ba8872294aaeaeed84b11f6a6b1062115af
磊 <839238345@qq.com>
2018-11-29 09:15:09 +0800

    更新类库信息

--- Infrastructure/EInfrastructure.Core.AliYun.Tbk/Config/AliConfig.cs
+++ Infrastructure/EInfrastructure.Core.AliYun.Tbk/Config/AliConfig.cs
@@ -3,6 +3,12 @@
     public class AliConfig
     {
         /// <summary>
+        /// 推广id
+        /// 格式：推广者_媒体_推广位
+        /// </summary>
+        public string Pid { get; set; }
+        
+        /// <summary>
         /// 淘口令使用
         /// </summary>
         public string AppKey { get; set; }
