cf7c0ba05fd5176a96a9917e506f39466d9b44f4
磊 <839238345@qq.com>
2019-01-31 18:01:12 +0800

    修改发布配置

--- Infrastructure/EInfrastructure.Core.AspNetCore/Exception/Ext.cs
+++ Infrastructure/EInfrastructure.Core.AspNetCore/Exception/Ext.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 namespace EInfrastructure.Core.AspNetCore.Exception
@@ -25,6 +29,41 @@ namespace EInfrastructure.Core.AspNetCore.Exception
                     options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                     options.SerializerSettings.DateFormatString = "yyyy-MM-dd HH:mm:ss";
                 }));
+        }
+
+        #endregion
+
+        #region 使用自定义配置
+
+        /// <summary>
+        /// 使用自定义配置
+        /// </summary>
+        /// <param name="env">环境信息</param>
+        /// <param name="useEnvironment">是否使用环境变量</param>
+        /// <param name="configPathList">自定义配置列表</param>
+        /// <returns></returns>
+        public static IConfigurationRoot UseAppsettings(this IHostingEnvironment env, bool useEnvironment = true,
+            List<string> configPathList = null)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            if (useEnvironment)
+            {
+                builder.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true)
+                    .AddEnvironmentVariables();
+            }
+
+            if (configPathList == null)
+            {
+                configPathList = new List<string>();
+            }
+
+            configPathList.ForEach(configPath => { builder.AddJsonFile(configPath, true, true); });
+
+            var config = builder.Build();
+
+            return config;
         }
 
         #endregion
