5ed0c97f34fa7b250e3cc6485c3f4d2bd8f23119
磊-700 <839238345@qq.com>
2019-02-02 17:01:03 +0800

    去除word方法中多余字段

--- Infrastructure/EInfrastructure.Core.Words/Startup.cs
+++ Infrastructure/EInfrastructure.Core.Words/Startup.cs
@@ -17,16 +17,8 @@ namespace EInfrastructure.Core.Words
         /// </summary>
         /// <param name="serviceCollection"></param>
         /// <param name="configuration"></param>
-        /// <param name="file"></param>
-        /// <param name="isCompleteName">是否输入完整的类名，默认：false，为true时则需要输入命名空间+类名（配置文件需要）</param>
-        /// <param name="action"></param>
-        /// <param name="errConfigAction">配置信息错误回调</param>
         public static IServiceCollection AddWords(this IServiceCollection serviceCollection,
-            IConfiguration configuration,
-            string file = "appsettings.json",
-            bool isCompleteName = false,
-            Action<ConfigAutoRegister> action = null,
-            Action<string> errConfigAction = null)
+            IConfiguration configuration)
         {
             serviceCollection.AddCustomerConfig<DictPinYinPathConfig>(
                 configuration.GetSection("EInfrastructure.Core.Words.Config.PinYin.DictPinYinPathConfig"),
