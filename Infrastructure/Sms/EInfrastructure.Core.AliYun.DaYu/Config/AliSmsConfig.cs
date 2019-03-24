f3a5c30c9893cbddc6bce93bd97ac05e8e327b2f
磊 <839238345@qq.com>
2019-03-24 19:18:30 +0800

    结构调整

---
+++ Infrastructure/Sms/EInfrastructure.Core.AliYun.DaYu/Config/AliSmsConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace EInfrastructure.Core.AliYun.DaYu.Config
+{
+    /// <summary>
+    /// 短信配置
+    /// </summary>
+    public class AliSmsConfig
+    {
+        /// <summary>
+        /// 签名名称
+        /// </summary>
+        public string SignName { get; set; }
+
+        /// <summary>
+        /// AccessKey ID
+        /// </summary>
+        public string AccessKey { get; set; }
+
+        /// <summary>
+        /// 秘钥参数
+        /// </summary>
+        public string EncryptionKey { get; set; }
+    }
+}
