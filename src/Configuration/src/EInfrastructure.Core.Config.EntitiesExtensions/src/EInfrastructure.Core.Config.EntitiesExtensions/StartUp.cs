e466867f0623661deb3639a6119d189f8b1cd924
磊 <839238345@qq.com>
2019-05-07 16:22:36 +0800

    解决 延迟加载问题

--- src/Configuration/src/EInfrastructure.Core.Config.EntitiesExtensions/src/EInfrastructure.Core.Config.EntitiesExtensions/StartUp.cs
+++ src/Configuration/src/EInfrastructure.Core.Config.EntitiesExtensions/src/EInfrastructure.Core.Config.EntitiesExtensions/StartUp.cs
@@ -1,20 +1,15 @@
 // Copyright (c) zhenlei520 All rights reserved.
 
-namespace EInfrastructure.Core.Configuration
+namespace EInfrastructure.Core.Config.EntitiesExtensions
 {
     public static class StartUp
     {
-        private static bool IsStartUp;
-
         /// <summary>
         /// 启用配置
         /// </summary>
         public static void Run()
         {
-            if (!IsStartUp)
-            {
-                IsStartUp = true;
-            }
+            Configuration.StartUp.Run();
         }
     }
 }
---
+++ src/Infrastructure/src/EInfrastructure.Core/StartUp.cs
@@ -0,0 +1,40 @@
+// Copyright (c) zhenlei520 All rights reserved.
+
+namespace EInfrastructure.Core
+{
+    public static class StartUp
+    {
+        private static bool IsStartUp;
+
+        /// <summary>
+        /// 启用配置
+        /// </summary>
+        public static void Run()
+        {
+            if (!IsStartUp)
+            {
+                IsStartUp = true;
+                Load();
+            }
+        }
+
+        #region private methods
+
+        /// <summary>
+        /// load配置类库
+        /// </summary>
+        private static void Load()
+        {
+            Config.CacheExtensions.StartUp.Run();
+            Config.CompressExtensions.StartUp.Run();
+            Config.EntitiesExtensions.StartUp.Run();
+            Config.IdentificationExtensions.StartUp.Run();
+            Config.SerializeExtensions.StartUp.Run();
+            Config.SmsExtensions.StartUp.Run();
+            Config.StorageExtensions.StartUp.Run();
+            Config.WordsExtensions.StartUp.Run();
+        }
+
+        #endregion
+    }
+}
