610e69ee039a1dda8b55bb50cd6dfdeb0205f637
磊 <839238345@qq.com>
2019-05-08 10:14:15 +0800

    修改版本与异常提示

--- src/Infrastructure/src/EInfrastructure.Core/Exception/BusinessException.cs
+++ src/Infrastructure/src/EInfrastructure.Core/Exception/BusinessException.cs
@@ -1,8 +1,11 @@
 ﻿// Copyright (c) zhenlei520 All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
+using EInfrastructure.Core.Config.SerializeExtensions;
 using EInfrastructure.Core.Config.SerializeExtensions.Interfaces;
 using EInfrastructure.Core.Configuration.Enum;
+using EInfrastructure.Core.Serialize.NewtonsoftJson;
 
 namespace EInfrastructure.Core.Exception
 {
@@ -12,20 +15,28 @@ namespace EInfrastructure.Core.Exception
     /// </summary>
     public class BusinessException : System.Exception
     {
-        private static IJsonProvider _jsonProvider;
+        private static IJsonService _jsonProvider;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="jsonProvider"></param>
+        public BusinessException(IJsonService jsonProvider = null)
+        {
+            _jsonProvider = jsonProvider ?? new JsonService(new List<IJsonProvider>()
+            {
+                new NewtonsoftJsonProvider()
+            });
+        }
 
         /// <summary>
         /// 业务异常
         /// </summary>
         /// <param name="code">状态码</param>
         /// <param name="content">异常详情</param>
-        public BusinessException(string content, int code = (int) HttpStatusEnum.Err)
+        public BusinessException(string content, int code = (int) HttpStatusEnum.Err) : base(
+            _jsonProvider.Serializer(new {code, content}))
         {
-            if (_jsonProvider == null)
-            {
-                _jsonProvider=new Serialize.NewtonsoftJson.NewtonsoftJsonProvider();
-            }
-            throw new System.Exception(_jsonProvider.Serializer(new {code, content}));
         }
     }
 }
