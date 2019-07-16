7a53bf8b1aeffc99440c4448d111ac53d8cc7eb9
听雨声 <zhenlei520@users.noreply.github.com>
2019-07-16 11:12:10 +0800

    Update CloneableClass.cs

--- src/Infrastructure/src/EInfrastructure.Core/HelpCommon/CloneableClass.cs
+++ src/Infrastructure/src/EInfrastructure.Core/HelpCommon/CloneableClass.cs
@@ -37,24 +37,13 @@ namespace EInfrastructure.Core.HelpCommon
         /// <returns></returns>
         public T DeepClone<T>(T obj)
         {
-            if (obj is string || obj.GetType().IsValueType)
-                return obj;
-
-            object retval = Activator.CreateInstance(obj.GetType());
-            FieldInfo[] fields = obj.GetType()
-                .GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.Instance);
-            foreach (var field in fields)
+            using (Stream objectStream = new MemoryStream())
             {
-                try
-                {
-                    field.SetValue(retval, DeepClone(field.GetValue(obj)));
-                }
-                catch
-                {
-                }
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(objectStream, t);
+                objectStream.Seek(0, SeekOrigin.Begin);
+                return (T) formatter.Deserialize(objectStream);
             }
-
-            return (T) retval;
         }
 
         #endregion
