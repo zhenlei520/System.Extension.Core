4ea080683b521c2848ae45fc6f17acfa3c95befa
磊磊 <wangzhenlei520@gmail.com>
2019-08-13 17:26:26 +0800

    合并冲突问题

--- src/Infrastructure/src/EInfrastructure.Core/HelpCommon/CloneableClass.cs
+++ src/Infrastructure/src/EInfrastructure.Core/HelpCommon/CloneableClass.cs
@@ -40,7 +40,7 @@ namespace EInfrastructure.Core.HelpCommon
             using (Stream objectStream = new MemoryStream())
             {
                 IFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(objectStream, t);
+                formatter.Serialize(objectStream, obj);
                 objectStream.Seek(0, SeekOrigin.Begin);
                 return (T) formatter.Deserialize(objectStream);
             }
