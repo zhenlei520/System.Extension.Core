40ae809873a91fe720b0f0b465a83375c6d02f23
磊磊 <839238345@qq.com>
2019-11-16 17:48:44 +0800

    consul支持注册多个

--- src/Compress/ICSharpCode/src/EInfrastructure.Core.Compress.ICSharpCode/obj/Debug/netstandard2.0/EInfrastructure.Core.Compress.ICSharpCode.AssemblyInfo.cs
+++ src/Compress/ICSharpCode/src/EInfrastructure.Core.Compress.ICSharpCode/obj/Debug/netstandard2.0/EInfrastructure.Core.Compress.ICSharpCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute("dotnet core 压缩包类库（父类ICSharpCode.SharpZipLib），通过进一步封装ICSharpCode.SharpZipLib实现压缩与解" +
 "压功能，中文乱码问题已修复")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-035")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-036")]
 [assembly: System.Reflection.AssemblyProductAttribute("EInfrastructure.Core.Compress.ICSharpCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EInfrastructure.Core.Compress.ICSharpCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
--- src/Sms/AliDaYu/src/EInfrastructure.Core.AliYun.DaYu/obj/Debug/netstandard2.0/EInfrastructure.Core.AliYun.DaYu.AssemblyInfo.cs
+++ src/Sms/AliDaYu/src/EInfrastructure.Core.AliYun.DaYu/obj/Debug/netstandard2.0/EInfrastructure.Core.AliYun.DaYu.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©zhenlei520 2018")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("dotnet core 阿里大鱼短信类库")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-035")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-036")]
 [assembly: System.Reflection.AssemblyProductAttribute("EInfrastructure.Core.AliYun.DaYu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EInfrastructure.Core.AliYun.DaYu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
