6a586eee34e49042dbc3fe2d0727c4630a721ec2
磊磊 <839238345@qq.com>
2019-11-18 20:41:10 +0800

    响应时增加当前时间返回

--- src/Web/AspNetCore/src/EInfrastructure.Core.AspNetCore/Api/ApiErrResult.cs
+++ src/Web/AspNetCore/src/EInfrastructure.Core.AspNetCore/Api/ApiErrResult.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) zhenlei520 All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using EInfrastructure.Core.AspNetCore.Api.Common;
 
 namespace EInfrastructure.Core.AspNetCore.Api
@@ -19,5 +20,22 @@ namespace EInfrastructure.Core.AspNetCore.Api
         public ApiErrResult(int code, string msg, object extend = null) : base(code, msg, extend)
         {
         }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="code">错误码</param>
+        /// <param name="msg">错误信息</param>
+        /// <param name="extend">扩展信息</param>
+        /// <param name="isReturnCurrentTime">是否返回方式时间</param>
+        public ApiErrResult(int code, string msg, object extend, bool isReturnCurrentTime) : base(code, msg, extend,
+            isReturnCurrentTime)
+        {
+        }
+
+        public override string Msg { get; set; }
+        public override int Code { get; set; }
+        public override object Extend { get; set; }
+        public override DateTime? CurrentTime { get; set; }
     }
 }
