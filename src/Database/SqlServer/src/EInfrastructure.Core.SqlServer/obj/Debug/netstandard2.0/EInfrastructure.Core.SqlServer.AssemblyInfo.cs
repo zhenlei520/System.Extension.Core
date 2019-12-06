d6a244b08bb677d644cf8635bc322553390a0a04
磊磊 <839238345@qq.com>
2019-12-06 18:07:46 +0800

    七牛上传时允许自定义上传回调信息

--- src/Database/SqlServer/src/EInfrastructure.Core.SqlServer/obj/Debug/netstandard2.0/EInfrastructure.Core.SqlServer.AssemblyInfo.cs
+++ src/Database/SqlServer/src/EInfrastructure.Core.SqlServer/obj/Debug/netstandard2.0/EInfrastructure.Core.SqlServer.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©zhenlei520 2018")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("dotnet core 使用sqlserver数据库实现")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-041")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-042")]
 [assembly: System.Reflection.AssemblyProductAttribute("EInfrastructure.Core.SqlServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EInfrastructure.Core.SqlServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
