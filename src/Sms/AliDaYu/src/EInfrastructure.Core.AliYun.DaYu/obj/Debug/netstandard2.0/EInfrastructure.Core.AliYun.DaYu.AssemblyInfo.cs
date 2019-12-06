6ab2397eccaff774f814eb9be422ca5a39e735ce
磊磊 <839238345@qq.com>
2019-12-06 12:41:25 +0800

    更新web

--- src/Sms/AliDaYu/src/EInfrastructure.Core.AliYun.DaYu/obj/Debug/netstandard2.0/EInfrastructure.Core.AliYun.DaYu.AssemblyInfo.cs
+++ src/Sms/AliDaYu/src/EInfrastructure.Core.AliYun.DaYu/obj/Debug/netstandard2.0/EInfrastructure.Core.AliYun.DaYu.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©zhenlei520 2018")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("dotnet core 阿里大鱼短信类库")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-040-beta")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-040")]
 [assembly: System.Reflection.AssemblyProductAttribute("EInfrastructure.Core.AliYun.DaYu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EInfrastructure.Core.AliYun.DaYu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
