903e650149e336fdcd9d9473aa1b711ebb2b44ef
磊磊 <839238345@qq.com>
2019-12-10 15:44:49 +0800

    重新发布一版，上一版久久不能通过审核

--- src/Tools/Words/src/EInfrastructure.Core.Words/obj/Debug/netstandard2.0/EInfrastructure.Core.Words.AssemblyInfo.cs
+++ src/Tools/Words/src/EInfrastructure.Core.Words/obj/Debug/netstandard2.0/EInfrastructure.Core.Words.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright ©zhenlei520 2018")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("dotnet core words，pinyin词语类库")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-045")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0-beta-046")]
 [assembly: System.Reflection.AssemblyProductAttribute("EInfrastructure.Core.Words")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EInfrastructure.Core.Words")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
