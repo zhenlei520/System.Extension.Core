64028823f57cbb015aa96b26514d1c358f13b151
牛延瞳 <niutong1995@163.com>
2020-04-08 15:01:55 +0800

    修改分页数据集合构造函数赋值

--- src/Configuration/src/EInfrastructure.Core.Config.Entities/Data/PageData.cs
+++ src/Configuration/src/EInfrastructure.Core.Config.Entities/Data/PageData.cs
@@ -20,13 +20,27 @@ namespace EInfrastructure.Core.Config.Entities.Data
         }
 
         /// <summary>
-        ///
+        /// 分页
         /// </summary>
         /// <param name="rowCount">总条数</param>
         /// <param name="data">数据集合</param>
-        /// <param name="extendedData">扩展信息</param>
+        public PageData(int rowCount, List<T> data)
+        {
+            RowCount = rowCount;
+            Data = data;
+        }
+
+        /// <summary>
+        /// 分页
+        /// </summary>
+        /// <param name="rowCount"></param>
+        /// <param name="data"></param>
+        /// <param name="extendedData"></param>
         public PageData(int rowCount, List<T> data, object extendedData)
         {
+            RowCount = rowCount;
+            Data = data;
+            ExtendedData = extendedData;
         }
 
         /// <summary>
@@ -45,7 +59,6 @@ namespace EInfrastructure.Core.Config.Entities.Data
         /// 扩展Data
         /// </summary>
         [JsonProperty(PropertyName = "extend_data")]
-        [JsonIgnore]
         public virtual object ExtendedData { get; set; }
     }
 }
