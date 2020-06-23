34c86677dca5828e9defefd1084d1f86ae9c1d93
磊磊 <839238345@qq.com>
2020-06-24 00:43:48 +0800

    增加图片鉴定接口

---
+++ src/Configuration/src/EInfrastructure.Core.Configuration/Ioc/Plugs/ContentCensor/IPictureCensorProvider.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) zhenlei520 All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace EInfrastructure.Core.Configuration.Ioc.Plugs.ContentCensor
+{
+    /// <summary>
+    /// 图片审查服务
+    /// </summary>
+    public interface IPictureCensorProvider: IIdentify, ISingleInstance
+    {
+
+    }
+}
