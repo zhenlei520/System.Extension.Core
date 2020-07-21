10cdd2fa2c68d7b62ad7bc565b5346eda0795ce7
磊磊 <839238345@qq.com>
2020-07-21 09:55:53 +0800

    参考abp的生成基于数据库的连续guid方法改编唯一方法帮助类

---
+++ src/Configuration/src/EInfrastructure.Core.Config.Entities/Ioc/IGuidGenerator.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) zhenlei520 All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using EInfrastructure.Core.Configuration.Ioc;
+
+namespace EInfrastructure.Core.Config.Entities.Ioc
+{
+    /// <summary>
+    /// Used to generate Ids.
+    /// </summary>
+    public interface IGuidGenerator : IDependency
+    {
+        /// <summary>
+        /// Creates a new <see cref="Guid"/>.
+        /// </summary>
+        Guid Create();
+    }
+}
---
+++ src/Database/MySql/src/EInfrastructure.Core.MySql/GuidGenerator.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) zhenlei520 All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using EInfrastructure.Core.Config.Entities.Ioc;
+using EInfrastructure.Core.Tools.Enumerations;
+using EInfrastructure.Core.Tools.Unique;
+
+namespace EInfrastructure.Core.MySql
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public class GuidGenerator : IGuidGenerator
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public Guid Create()
+        {
+            return UniqueCommon.Guids(SequentialGuidType.SequentialAsString);
+        }
+    }
+}
