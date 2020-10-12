4f4c813dd818bb445b969ae587e11fafdae9fe4e
磊磊 <839238345@qq.com>
2020-10-13 00:05:33 +0800

    增加BloomFilter 参考来源https://sourceforge.net/projects/bloomfilternet/

---
+++ src/Infrastructure/src/EInfrastructure.Core.Tools/Extensions/BloomFilter/MathUtils.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) zhenlei520 All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+// 参考来源：https://sourceforge.net/projects/bloomfilternet/
+
+namespace EInfrastructure.Core.Tools.Extensions.BloomFilter
+{
+	internal static class MathUtils
+	{
+		/// <summary>
+		/// Compute the absolute value of an integer
+		/// </summary>
+		/// <param name="n"></param>
+		/// <returns></returns>
+		public static int Abs( int n )
+		{
+			if (n >= 0) return n;
+			else return -n;
+		}
+	}
+}
