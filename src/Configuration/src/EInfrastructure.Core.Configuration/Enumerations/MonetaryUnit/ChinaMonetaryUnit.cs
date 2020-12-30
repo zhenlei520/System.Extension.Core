1d49d28613321596342d0fc8649e8df585e75e8a
磊磊 <839238345@qq.com>
2020-12-30 23:07:56 +0800

    新增基本货币单位

---
+++ src/Configuration/src/EInfrastructure.Core.Configuration/Enumerations/MonetaryUnit/ChinaMonetaryUnit.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) zhenlei520 All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using EInfrastructure.Core.Configuration.Enumerations.SeedWork;
+
+namespace EInfrastructure.Core.Configuration.Enumerations.MonetaryUnit
+{
+    /// <summary>
+    /// 中国货币单位
+    /// </summary>
+    public class ChinaMonetaryUnit : Enumeration
+    {
+        /// <summary>
+        /// 元
+        /// </summary>
+        public static ChinaMonetaryUnit Yuan = new ChinaMonetaryUnit(1, "yuan");
+
+        /// <summary>
+        /// 角
+        /// </summary>
+        public static ChinaMonetaryUnit Dime = new ChinaMonetaryUnit(2, "dime");
+
+        /// <summary>
+        /// 分
+        /// </summary>
+        public static ChinaMonetaryUnit Cent = new ChinaMonetaryUnit(3, "cent");
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        public ChinaMonetaryUnit(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
---
+++ src/Configuration/src/EInfrastructure.Core.Configuration/Enumerations/MonetaryUnit/UsaMonetaryUnit.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) zhenlei520 All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using EInfrastructure.Core.Configuration.Enumerations.SeedWork;
+
+namespace EInfrastructure.Core.Configuration.Enumerations.MonetaryUnit
+{
+    /// <summary>
+    /// 美国货币单位
+    /// </summary>
+    public class UsaMonetaryUnit : Enumeration
+    {
+        /// <summary>
+        /// 美元
+        /// </summary>
+        public static UsaMonetaryUnit Dollar = new UsaMonetaryUnit(1, "dollar");
+
+        /// <summary>
+        /// 美分
+        /// </summary>
+        public static UsaMonetaryUnit Cent = new UsaMonetaryUnit(2, "cent");
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        public UsaMonetaryUnit(int id, string name) : base(id, name)
+        {
+        }
+    }
+}
